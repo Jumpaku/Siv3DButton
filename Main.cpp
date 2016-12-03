09fec77982310b4fcb4af58394442801a7a46ded
Jumpaku <Jumpaku@users.noreply.github.com>
2016-12-03 16:37:37 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -75,9 +75,9 @@ void Main()
 
     ButtonManager buttonManager{inclement, declement};
 
-	while (System::Update()) {
-            buttonManager.update();
-            buttonManager.draw();
-            font(L"number = " + ToString(number)).drawCenter(320, 30);
-	}
+    while (System::Update()) {
+        buttonManager.update();
+        buttonManager.draw();
+        font(L"number = " + ToString(number)).drawCenter(320, 30);
+    }
 }
